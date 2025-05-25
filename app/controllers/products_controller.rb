4b18c36123f211b469cea90bff6d6e4a5b523c8b
ChaeyeonYoo <102454450+ChaeyeonYoo@users.noreply.github.com>
2025-05-26 05:39:44 +0900

    Remove duplicate CreateSessions migration

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,6 +24,23 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    @product = Product.find(params[:id])
+  end
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def product_params
+    params.expect(product: [ :name ])
+  end
+
+  def edit
   end
 
   def update
