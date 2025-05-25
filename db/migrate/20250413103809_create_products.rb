10c9f471fafad52bec8390d98f6ca95b2e02e5cd
ChaeyeonYoo <102454450+ChaeyeonYoo@users.noreply.github.com>
2025-05-26 05:14:30 +0900

    임시 커밋: schema.rb 변경 등

--- db/migrate/20250413103809_create_products.rb
+++ db/migrate/20250413103809_create_products.rb
@@ -1,8 +1,7 @@
-class CreateProducts < ActiveRecord::Migration[8.0]
+class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
       t.string :name
-
       t.timestamps
     end
   end
